0c631d447da5b73a3887146408ee8c535a1f4d99
Ana Clara Guerra <ana.guerrads@hotmail.com>
2021-06-03 20:10:04 -0300

    Final Commit

--- app/src/main/java/br/com/appcalculadoraimcudemy/MainActivity.kt
+++ app/src/main/java/br/com/appcalculadoraimcudemy/MainActivity.kt
@@ -12,6 +12,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
+     binding.btCalcular.setOnClickListener {
+
+         val num1 = binding.rlEtNota1
+         val num2 = binding.rlEtNota2
+         val num3 = binding.rlEtNota3
+         val num4 = binding.rlEtNota4
+         val numfaltas = binding.etFaltas
+         val resultado = binding.tvResultado
+
+         val nota1 = Integer.parseInt(num1.text.toString())
+         val nota2 = Integer.parseInt(num2.text.toString())
+         val nota3 = Integer.parseInt(num3.text.toString())
+         val nota4 = Integer.parseInt(num4.text.toString())
+         val faltas = Integer.parseInt(numfaltas.text.toString())
+
+         val media = (nota1 + nota2 + nota3 + nota4)/4
+
+         if((media >= 6) && (faltas <= 10)) {
+
+             resultado.setText("O Aluno foi APROVADO com a média $media e a quantidade de faltas $faltas!")
+             resultado.setTextColor(getColor(R.color.result_green))
+
+         } else if((media >= 6) && (faltas > 10)) {
+
+             resultado.setText("O Aluno foi REPROVADO com a média $media e a quantidade de faltas $faltas!")
+             resultado.setTextColor(getColor(R.color.red))
+
+         } else if(media < 6) {
+
+            resultado.setText("O Aluno foi REPROVADO com a média $media e a quantidade de faltas $faltas!")
+             resultado.setTextColor(getColor(R.color.red))
+         }
+     }
+    }
 }
